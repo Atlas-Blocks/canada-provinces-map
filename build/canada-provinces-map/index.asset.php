061b7b404a9adf3ef266261baaae7efdcc86c859
Evan <evanh@cs.dal.ca>
2026-05-20 22:53:23 -0300

    Initial Canada map block

---
+++ build/canada-provinces-map/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '18c97a2b6ec7e6f2dc93');
---
+++ build/canada-provinces-map/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fcd53f81c15be59639ef');
